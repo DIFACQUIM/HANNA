e75029f0046f5b4998764be4a560c3b0caf04d48
Ana Luisa Chávez <58413133+AnaChavezHdz@users.noreply.github.com>
2023-10-05 16:52:04 -0600

    Add files via upload

--- Graphs.py
+++ Graphs.py
@@ -77,7 +77,7 @@ e_map = {
 }
 
 
-def from_smiles(smiles: str, with_hydrogen: bool = False,
+def from_smiles(x_smiles: str, with_hydrogen: bool = False,
                 kekulize: bool = False, y: float) -> 'torch_geometric.data.Data':
     r"""Converts a SMILES string to a :class:`torch_geometric.data.Data`
     instance.
@@ -94,55 +94,59 @@ def from_smiles(smiles: str, with_hydrogen: bool = False,
     from torch_geometric.data import Data
 
     RDLogger.DisableLog('rdApp.*')
+    
+    for (smiles, y_val) in zip(x_smiles, y):
+	    mol = Chem.MolFromSmiles(smiles)
 
-    mol = Chem.MolFromSmiles(smiles)
+	    if mol is None:
+		mol = Chem.MolFromSmiles('')
+	    if with_hydrogen:
+		mol = Chem.AddHs(mol)
+	    if kekulize:
+		Chem.Kekulize(mol)
 
-    if mol is None:
-        mol = Chem.MolFromSmiles('')
-    if with_hydrogen:
-        mol = Chem.AddHs(mol)
-    if kekulize:
-        Chem.Kekulize(mol)
+	    xs = []
+	    for atom in mol.GetAtoms():
+		x = []
+		x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
+		x.append(x_map['chirality'].index(str(atom.GetChiralTag())))
+		x.append(x_map['degree'].index(atom.GetTotalDegree()))
+		x.append(x_map['formal_charge'].index(atom.GetFormalCharge()))
+		x.append(x_map['num_hs'].index(atom.GetTotalNumHs()))
+		x.append(x_map['num_radical_electrons'].index(
+		    atom.GetNumRadicalElectrons()))
+		x.append(x_map['hybridization'].index(str(atom.GetHybridization())))
+		x.append(x_map['is_aromatic'].index(atom.GetIsAromatic()))
+		x.append(x_map['is_in_ring'].index(atom.IsInRing()))
+		xs.append(x)
 
-    xs = []
-    for atom in mol.GetAtoms():
-        x = []
-        x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
-        x.append(x_map['chirality'].index(str(atom.GetChiralTag())))
-        x.append(x_map['degree'].index(atom.GetTotalDegree()))
-        x.append(x_map['formal_charge'].index(atom.GetFormalCharge()))
-        x.append(x_map['num_hs'].index(atom.GetTotalNumHs()))
-        x.append(x_map['num_radical_electrons'].index(
-            atom.GetNumRadicalElectrons()))
-        x.append(x_map['hybridization'].index(str(atom.GetHybridization())))
-        x.append(x_map['is_aromatic'].index(atom.GetIsAromatic()))
-        x.append(x_map['is_in_ring'].index(atom.IsInRing()))
-        xs.append(x)
+	    x = torch.tensor(xs, dtype=torch.long).view(-1, 9)
 
-    x = torch.tensor(xs, dtype=torch.long).view(-1, 9)
+	    edge_indices, edge_attrs = [], []
+	    for bond in mol.GetBonds():
+		i = bond.GetBeginAtomIdx()
+		j = bond.GetEndAtomIdx()
 
-    edge_indices, edge_attrs = [], []
-    for bond in mol.GetBonds():
-        i = bond.GetBeginAtomIdx()
-        j = bond.GetEndAtomIdx()
+		e = []
+		e.append(e_map['bond_type'].index(str(bond.GetBondType())))
+		e.append(e_map['stereo'].index(str(bond.GetStereo())))
+		e.append(e_map['is_conjugated'].index(bond.GetIsConjugated()))
 
-        e = []
-        e.append(e_map['bond_type'].index(str(bond.GetBondType())))
-        e.append(e_map['stereo'].index(str(bond.GetStereo())))
-        e.append(e_map['is_conjugated'].index(bond.GetIsConjugated()))
+		edge_indices += [[i, j], [j, i]]
+		edge_attrs += [e, e]
 
-        edge_indices += [[i, j], [j, i]]
-        edge_attrs += [e, e]
+	    edge_index = torch.tensor(edge_indices)
+	    edge_index = edge_index.t().to(torch.long).view(2, -1)
+	    edge_attr = torch.tensor(edge_attrs, dtype=torch.long).view(-1, 3)
 
-    edge_index = torch.tensor(edge_indices)
-    edge_index = edge_index.t().to(torch.long).view(2, -1)
-    edge_attr = torch.tensor(edge_attrs, dtype=torch.long).view(-1, 3)
+	    if edge_index.numel() > 0:  # Sort indices.
+		perm = (edge_index[0] * x.size(0) + edge_index[1]).argsort()
+		edge_index, edge_attr = edge_index[:, perm], edge_attr[perm]
+	    
+	    # construct label tensor
+	    y_tensor = torch.tensor(np.array([y_val]), dtype = torch.float)
 
-    if edge_index.numel() > 0:  # Sort indices.
-        perm = (edge_index[0] * x.size(0) + edge_index[1]).argsort()
-        edge_index, edge_attr = edge_index[:, perm], edge_attr[perm]
-
-    return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, smiles=smiles, y=y)
+    return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, smiles=smiles, y=y_tensor)
 
 
 def to_smiles(data: 'torch_geometric.data.Data',
